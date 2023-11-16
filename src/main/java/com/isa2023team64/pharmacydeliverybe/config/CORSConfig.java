93211b41151a8a4f236fa254f8f33a86258b91d5
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-11-16 13:59:50 +0100

    Configured CORS Policy.

---
+++ src/main/java/com/isa2023team64/pharmacydeliverybe/config/CORSConfig.java
@@ -0,0 +1,18 @@
+package com.isa2023team64.pharmacydeliverybe.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
