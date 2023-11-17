f96b65b619a0e69a4ab4f2334fd80be80ab3036c
kirdatatjana <kirda.tatjana@gmail.com>
2023-11-17 21:06:55 +0100

    Revert "Revert "Merge branch 'feat/user-registration' into development""
    This reverts commit 2a615e6641099b33ae6764db573332791ac19853.

--- src/main/java/com/isa2023team64/pharmacydeliverybe/dto/RegisteredUserRequestDTO.java
+++ src/main/java/com/isa2023team64/pharmacydeliverybe/dto/RegisteredUserRequestDTO.java
@@ -3,34 +3,34 @@ package com.isa2023team64.pharmacydeliverybe.dto;
 import com.isa2023team64.pharmacydeliverybe.model.RegisteredUser;
 
 public class RegisteredUserRequestDTO {
-    private String username;
     private String email;
     private String password;
     private String firstName;
     private String lastName;
+    private String city;
+    private String country;
+    private String phoneNumber;
+    private String workplace;
+    private String company;
 
     public RegisteredUserRequestDTO() {
         super();
     }
 
     public RegisteredUserRequestDTO(RegisteredUser registeredUser) {
-        this(registeredUser.getUsername(), registeredUser.getEmail(), registeredUser.getPassword(), registeredUser.getFirstName(), registeredUser.getLastName());
+        this(registeredUser.getEmail(), registeredUser.getPassword(), registeredUser.getFirstName(), registeredUser.getLastName(),registeredUser.getCity(),registeredUser.getCountry(),registeredUser.getPhoneNumber(),registeredUser.getWorkplace(),registeredUser.getCompany());
     }
 
-    public RegisteredUserRequestDTO(String username, String email, String password, String firstName, String lastName) {
-        this.username = username;
+    public RegisteredUserRequestDTO(String email, String password, String firstName, String lastName, String city, String country,String phoneNumber, String workplace, String company) {
         this.email = email;
         this.password = password;        
         this.firstName = firstName;
         this.lastName = lastName;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
+        this.city = city;
+        this.country = country;
+        this.phoneNumber = phoneNumber;
+        this.workplace = workplace;
+        this.company = company;
     }
 
     public String getEmail() {
@@ -62,5 +62,45 @@ public class RegisteredUserRequestDTO {
     }
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getWorkplace() {
+        return workplace;
+    }
+
+    public void setWorkplace(String workplace) {
+        this.workplace = workplace;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
     }
 }
