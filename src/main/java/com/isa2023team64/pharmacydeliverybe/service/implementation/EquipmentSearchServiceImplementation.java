acbc525b6b6d03737fe5d816fa57793cc8156dd9
milossdjuric <milossdjuric@yahoo.com>
2023-11-18 22:29:42 +0100

    fix: Change Equipment Search DTO to ResponseDTP

--- src/main/java/com/isa2023team64/pharmacydeliverybe/service/implementation/EquipmentSearchServiceImplementation.java
+++ src/main/java/com/isa2023team64/pharmacydeliverybe/service/implementation/EquipmentSearchServiceImplementation.java
@@ -7,7 +7,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.isa2023team64.pharmacydeliverybe.dto.EquipmentRequestDTO;
+import com.isa2023team64.pharmacydeliverybe.dto.EquipmentResponseDTO;
 import com.isa2023team64.pharmacydeliverybe.dto.EquipmentSearchFilterDTO;
 import com.isa2023team64.pharmacydeliverybe.model.Equipment;
 import com.isa2023team64.pharmacydeliverybe.model.EquipmentSearchFilter;
@@ -36,7 +36,7 @@ public class EquipmentSearchServiceImplementation implements EquipmentSearchServ
     @Autowired
     private PagedResultConverter pagedResultConverter;
 
-    public PagedResult<EquipmentRequestDTO> search(EquipmentSearchFilterDTO filter) {
+    public PagedResult<EquipmentResponseDTO> search(EquipmentSearchFilterDTO filter) {
         List<Equipment> equipments = equipmentRepository.findAll();
 
         EquipmentSearchFilter searchFilter = new EquipmentSearchFilter(
@@ -45,11 +45,11 @@ public class EquipmentSearchServiceImplementation implements EquipmentSearchServ
         equipments = filterService.filter(equipments, searchFilter);
         equipments = sortService.sort(equipments, filter.getSortCriteria());
 
-        List<EquipmentRequestDTO> equipmentDTOs = equipments.stream()
-                .map(equipment -> modelMapper.map(equipment, EquipmentRequestDTO.class))
+        List<EquipmentResponseDTO> equipmentDTOs = equipments.stream()
+                .map(equipment -> modelMapper.map(equipment, EquipmentResponseDTO.class))
                 .collect(Collectors.toList());
 
-        PagedResult<EquipmentRequestDTO> equipmentPage = pagedResultConverter.convertToPagedResult(
+        PagedResult<EquipmentResponseDTO> equipmentPage = pagedResultConverter.convertToPagedResult(
             equipmentDTOs, filter.getPage(), filter.getPageSize());
 
         return equipmentPage;
