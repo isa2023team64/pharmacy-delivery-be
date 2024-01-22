45b5344888365bc01ee996a59d617ea11cdfa54a
kirdatatjana <kirda.tatjana@gmail.com>
2024-01-22 13:24:06 +0100

    added kafka listener

---
+++ src/main/java/com/isa2023team64/pharmacydeliverybe/listener/KafkaConsumer.java
@@ -0,0 +1,19 @@
+package com.isa2023team64.pharmacydeliverybe.listener;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+/**
+ * @author Febin Malik
+ * @Mobile +91 9656668856
+ * @Email codewithfebinmalik@gmail.com
+ */
+
+@Configuration
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "Kafka-producer-topic", groupId = "Consumer-Group-1")
+    public void listenGroupFoo(String message) {
+        System.out.println("Received Message in group foo: " + message);
+    }
+}
