32f3235ee6fbce3f7565377c7d152ddc70b12630
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2024-01-24 16:57:23 +0100

    Update Contract.java

--- src/main/java/com/isa2023team64/pharmacydeliverybe/model/Contract.java
+++ src/main/java/com/isa2023team64/pharmacydeliverybe/model/Contract.java
@@ -1,23 +1,25 @@
-// package com.isa2023team64.pharmacydeliverybe.model;
+package com.isa2023team64.pharmacydeliverybe.model;
 
-// import java.util.Collection;
+import java.util.Collection;
 
-// import jakarta.persistence.CascadeType;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.OneToOne;
-// import lombok.Getter;
-// import lombok.NoArgsConstructor;
-// import lombok.Setter;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-// @Entity
-// @Getter
-// @Setter
-// @NoArgsConstructor
-// public class Contract extends GenericEntity {
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Contract extends GenericEntity {
     
-//     @OneToOne(cascade = CascadeType.ALL)
-//     public Hospital hospital;
+    @OneToOne(cascade = CascadeType.ALL)
+    public Hospital hospital;
 
-//     public Collection<Equipment> equipments;
+    @OneToMany
+    public Collection<Equipment> equipments;
 
-// }
+}
