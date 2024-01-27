f29bb53fedc3f87144b03ffe6943b718c58ad0e9
milossdjuric <milossdjuric@yahoo.com>
2024-01-27 11:41:11 +0100

    temporary fix: Enable API calls ignore

--- src/main/java/com/isa2023team64/pharmacydeliverybe/config/WebSecurityConfig.java
+++ src/main/java/com/isa2023team64/pharmacydeliverybe/config/WebSecurityConfig.java
@@ -171,7 +171,7 @@ public class WebSecurityConfig {
     			
     			// Ovim smo dozvolili pristup statickim resursima aplikacije
     			.requestMatchers(HttpMethod.GET, "/", "/swagger-ui/*","/v3/api-docs/*", "/webjars/*", "/*.html", "favicon.ico",
-    			"/*/*.html", "/*/*.css", "/*/*.js");	 
+    			"/*/*.html", "/*/*.css", "/*/*.js", "/api/**");	 
 
     }
 
