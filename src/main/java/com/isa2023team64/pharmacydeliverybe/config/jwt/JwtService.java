0893cf19ed1bf859a2d834b45caeb643ed7b8446
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2024-01-30 15:31:10 +0100

    Fixed jwt token

--- src/main/java/com/isa2023team64/pharmacydeliverybe/config/jwt/JwtService.java
+++ src/main/java/com/isa2023team64/pharmacydeliverybe/config/jwt/JwtService.java
@@ -3,11 +3,17 @@ package com.isa2023team64.pharmacydeliverybe.config.jwt;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import com.isa2023team64.pharmacydeliverybe.model.Role;
+import com.isa2023team64.pharmacydeliverybe.model.User;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtParser;
@@ -18,13 +24,27 @@ import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
+	@Value("pharmacy-delivery-be")
+	private String APP_NAME;
 
-    private static final String SECRET_KEY = "4A8F92B0E7D3C1A6F5B8E9D2C5A8F1B2D4C7A9F8B1D3C6A7F0B2E5D8C1A4F7E2";
-    private static final Double hoursValid = 0.1;
+	private static final String AUDIENCE_WEB = "web";
+
+    private static final String SECRET_KEY = "1E9C4A0D68F73B8C21A6D05B8C9F2E7A8B2D1C4A9F3C6A7F0B2E5D8C1A4F7E2B5D8C1A9F3E6A7F0B2E5D8C1A4F7E2";
+    private static final Double hoursValid = 0.5;
     private static final Integer millisecondsValid = (int)(1000 * 60 * 60 * hoursValid);
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
+    }
+
+    public String generateToken(User user) {
+        List<String> roles = user.getRoles().stream()
+                .map(Role::getName)
+                .collect(Collectors.toList());
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("roles", roles);
+        claims.put("id", user.getId());
+        return generateToken(claims, user);
     }
 
     public String generateToken(UserDetails userDetails) {
@@ -32,13 +52,14 @@ public class JwtService {
     }
 
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
-        return Jwts
-            .builder()
+        return Jwts.builder()
+            .setIssuer(APP_NAME)
             .setClaims(extraClaims)
             .setSubject(userDetails.getUsername())
             .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setAudience(AUDIENCE_WEB)
             .setExpiration(new Date(System.currentTimeMillis() + millisecondsValid))
-            .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+            .signWith(getSignInKey(), SignatureAlgorithm.HS512)
             .compact();
     }
 
